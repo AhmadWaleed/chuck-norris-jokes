d9d33d3d8e02df6a227262a386bb19c275a6654d
Ahmed waleed <ahmed_waleed1@hotmail.com>
2019-03-17 16:40:13 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,14 +2,14 @@
 
 namespace AhmadWaleed\ChuckNorrisJokes;
 
-class JokeFactory 
+class JokeFactory
 {
     /**
      * @var array
      */
     private $jokes = [
         'Chuck Norris counted to infinity... Twice.',
-        'Chuck Norris doesn\'t shower, he only takes blood baths.'
+        'Chuck Norris doesn\'t shower, he only takes blood baths.',
     ];
 
     public function __construct(array $jokes = null)
